54db66374afce30a40cedc037cf0e5743c276ff9
Chunyolin <jeff042099@gmail.com>
2018-07-14 17:53:51 +0800

    add: simple menu select

--- pycurses.py
+++ pycurses.py
@@ -1,92 +1,46 @@
-import sys,os
+import sys, os
 import curses
+from curses import wrapper
 
-def draw_menu(stdscr):
-    k = 0
-    cursor_x = 0
-    cursor_y = 0
+class window():
+    def __init__(self, win, items):
+        self.win = win
+        self.items = items
+        self.cur_cursor = 0
 
-    # Clear and refresh the screen for a blank canvas
-    stdscr.clear()
-    stdscr.refresh()
+    def draw(self):
+        for idx, item in enumerate(self.items):
+            if idx == self.cur_cursor:
+                self.win.addstr(idx, 0, item, curses.A_REVERSE)
+            else:
+                self.win.addstr(idx, 0, item)
 
-    # Start colors in curses
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    def down(self):
+        if self.cur_cursor < len(self.items)-1:
+            self.cur_cursor += 1
+        else:
+            self.cur_cursor = len(self.items)-1
 
-    # Loop where k is the last character pressed
-    while (k != ord('q')):
+    def up(self):
+        if self.cur_cursor > 0:
+            self.cur_cursor -= 1
+        else:
+            self.cur_cursor = 0
 
-        # Initialization
-        stdscr.clear()
-        height, width = stdscr.getmaxyx()
 
-        if k == curses.KEY_DOWN:
-            cursor_y = cursor_y + 1
-        elif k == curses.KEY_UP:
-            cursor_y = cursor_y - 1
-        elif k == curses.KEY_RIGHT:
-            cursor_x = cursor_x + 1
-        elif k == curses.KEY_LEFT:
-            cursor_x = cursor_x - 1
-
-        cursor_x = max(0, cursor_x)
-        cursor_x = min(width-1, cursor_x)
-
-        cursor_y = max(0, cursor_y)
-        cursor_y = min(height-1, cursor_y)
-
-        # Declaration of strings
-        title = "Curses example"[:width-1]
-        subtitle = "Written by Clay McLeod"[:width-1]
-        keystr = "Last key pressed: {}".format(k)[:width-1]
-        statusbarstr = "Press 'q' to exit | STATUS BAR | Pos: {}, {}".format(cursor_x, cursor_y)
-        if k == 0:
-            keystr = "No key press detected..."[:width-1]
-
-        # Centering calculations
-        start_x_title = int((width // 2) - (len(title) // 2) - len(title) % 2)
-        start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
-        start_x_keystr = int((width // 2) - (len(keystr) // 2) - len(keystr) % 2)
-        start_y = int((height // 2) - 2)
-
-        # Rendering some text
-        whstr = "Width: {}, Height: {}".format(width, height)
-        stdscr.addstr(0, 0, whstr, curses.color_pair(1))
-
-        # Render status bar
-        stdscr.attron(curses.color_pair(3))
-        stdscr.addstr(height-1, 0, statusbarstr)
-        stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
-        stdscr.attroff(curses.color_pair(3))
-
-        # Turning on attributes for title
-        stdscr.attron(curses.color_pair(2))
-        stdscr.attron(curses.A_BOLD)
-
-        # Rendering title
-        stdscr.addstr(start_y, start_x_title, title)
-
-        # Turning off attributes for title
-        stdscr.attroff(curses.color_pair(2))
-        stdscr.attroff(curses.A_BOLD)
-
-        # Print rest of text
-        stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
-        stdscr.addstr(start_y + 3, (width // 2) - 2, '-' * 4)
-        stdscr.addstr(start_y + 5, start_x_keystr, keystr)
-        stdscr.move(cursor_y, cursor_x)
-
-        # Refresh the screen
-        stdscr.refresh()
-
-        # Wait for next input
-        k = stdscr.getch()
-
-def main():
-    curses.wrapper(draw_menu)
-
-if __name__ == "__main__":
-    main()
+def main(stdscr):
+    user_input = 0
+    curses.curs_set(False)
+    cur_window = window(stdscr, ["aaaaaa", "bbbbbb"])
+    cur_window.draw()
+    while user_input != ord('q'):
+        user_input = cur_window.win.getch()
+        if user_input == curses.KEY_DOWN:
+            cur_window.down()
+        elif user_input == curses.KEY_UP:
+            cur_window.up()
+        elif user_input == curses.KEY_ENTER:
+            cur_window.win.clear()
+        cur_window.draw()
+            
+wrapper(main)
