f73e5eb3ca197e1341300223470781c1dd3a131f
Chunyolin <r04922108@csie.ntu.edu.tw>
2018-07-16 23:09:28 +0800

    refactoring: create subwinitem

--- window/item/__init__.py
+++ window/item/__init__.py
@@ -1,1 +1,2 @@
-from .item import Item, BoolItem, MenuItem
+from .item import Item, BoolItem
+from .subwinitem import MenuItem
--- window/item/item.py
+++ window/item/item.py
@@ -19,6 +19,7 @@ class Item(metaclass=abc.ABCMeta):
     def help_str(self):
         pass
 
+    @property
     @abc.abstractclassmethod
     def value(self):
         pass
@@ -54,50 +55,3 @@ class BoolItem(Item):
     @value.setter
     def value(self, value):
         self._value = value
-
-class SubwinItem(Item):
-    def __init__(self):
-        pass
-
-    @abc.abstractclassmethod
-    def get_subwin(self):
-        pass
-
-    def toggle(self):
-        return Window.ENTER
-
-class MenuItem(SubwinItem):
-    def __init__(self, symbol, options=None, defaults=None, help_str=""):
-        self.symbol = symbol
-        if defaults:
-            assert set(defaults).issubset(set(options))
-        self.options = options
-        self.defaults = defaults
-        self._help_str = help_str
-        self.init_subwin()
-
-    def get_subwin(self):
-        return self.subwin
-
-    def init_subwin(self):
-        win = curses.newwin(curses.LINES, curses.COLS)
-        win.keypad(True)
-        subwin = window.MenuWindow(win)
-        for option in self.options:
-            if self.defaults and option in self.defaults:
-                subwin.add_bool(option, default=True)
-            else:
-                subwin.add_bool(option, default=False)
-        self.subwin = subwin
-
-    def str(self):
-        symbol_str = self.symbol
-        return "-->  {}".format(symbol_str)
-
-    @property 
-    def help_str(self):
-        return self._help_str
-
-    @property  
-    def value(self):
-        return {item.symbol: item.value for item in self.subwin.items}
